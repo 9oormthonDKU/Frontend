8002b7d920287035f24795e39ebc53f18cedd355
sep037 <tmddms0307@dankook.ac.kr>
2024-11-06 16:55:34 +0900

    feat(vote) : 데이터 추가
    #6

--- goormthonfe/lib/controllers/vote_controller.dart
+++ goormthonfe/lib/controllers/vote_controller.dart
@@ -6,6 +6,10 @@ class VoteController extends ChangeNotifier {
     UserVoteModel(name: '최상경', age: '23세', job: '바리스타', status: 'pending'),
     UserVoteModel(name: '김준현', age: '32세', job: '자영업', status: 'pending'),
     // 추가 사용자
+    UserVoteModel(name: '김민지', age: '22세', job: '대학생', status: 'pending'),
+    UserVoteModel(name: '강호준', age: '28세', job: '회사원', status: 'pending'),
+    UserVoteModel(name: '최예원', age: '23세', job: '회사원', status: 'pending'),
+    UserVoteModel(name: '권진호', age: '23세', job: '대학생', status: 'pending'),
   ];
 
   void acceptVote(int index) {
